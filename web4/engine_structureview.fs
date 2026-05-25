1eb7802c88b26990e00c9a85a8b83001165fb6d4
auraecosystem <sandraisom@icloud.com>
2026-05-25 14:39:37 +0100

    Create engine_structureview.fs

---
+++ web4/engine_structureview.fs
@@ -0,0 +1,17 @@
+web4/
+│
+├── runtime/
+│   ├── main.sj
+│   ├── runtime.sj
+│   ├── loader.sj
+│   └── msgsend.sj
+│
+├── app/
+│   ├── Main.j
+│   ├── AppController.j
+│   └── Views/
+│       └── HomeView.j
+│
+├── index.html
+│
+└── modules/
