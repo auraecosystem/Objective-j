e6e6f3500fd20827afd3c26de51b382d71fde3ec
auraecosystem <sandraisom@icloud.com>
2026-05-25 14:53:00 +0100

    Create __init__.glsl

---
+++ web4/runtime/sj/__init__.glsl
@@ -0,0 +1,21 @@
+precision mediump float;
+
+uniform float time;
+uniform vec2 resolution;
+
+void main() {
+
+```
+vec2 uv =
+    gl_FragCoord.xy / resolution;
+
+vec3 color =
+    0.5 + 0.5 * cos(
+        time + uv.xyx + vec3(0,2,4)
+    );
+
+gl_FragColor =
+    vec4(color, 1.0);
+```
+
+}
